507f17f56f88e3e5b6bb3a2bf727757a70f91e38
rsjavasolutions <stan.robert.1986@gmail.com>
2020-04-20 20:03:59 +0200

    Created class EntityBase

--- src/main/java/pl/rsjavasolutions/dtoquiz/model/EntityBase.java
+++ src/main/java/pl/rsjavasolutions/dtoquiz/model/EntityBase.java
@@ -1,4 +1,15 @@
 package pl.rsjavasolutions.dtoquiz.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class EntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
 }

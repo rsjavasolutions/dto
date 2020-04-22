759a254c5107f44bbdf7a75e2d2d5c7fe5e75f0f
rsjavasolutions <stan.robert.1986@gmail.com>
2020-04-22 16:43:52 +0200

    Finished class QuestionMapper

--- src/main/java/pl/rsjavasolutions/dtoquiz/dto/mapper/QuestionMapper.java
+++ src/main/java/pl/rsjavasolutions/dtoquiz/dto/mapper/QuestionMapper.java
@@ -1,4 +1,41 @@
 package pl.rsjavasolutions.dtoquiz.dto.mapper;
 
+import org.springframework.stereotype.Component;
+import pl.rsjavasolutions.dtoquiz.dto.QuestionDto;
+import pl.rsjavasolutions.dtoquiz.model.Question;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+@Component
 public class QuestionMapper {
+
+    private QuestionDto mapToQuestionDto(Question question){
+        QuestionDto questionDto = new QuestionDto();
+        questionDto.setId(question.getId());
+        questionDto.setContent(question.getContent());
+        questionDto.setAnswers(getAllAnswers(question));
+        questionDto.setCorrectAnswer(getCorrectAnswer(question));
+        return questionDto;
+    }
+
+    private List<String> getAllAnswers(Question question){
+        return question.getAnswerList().stream()
+                .map(answer -> answer.getName())
+                .collect(Collectors.toList());
+    }
+
+    private String getCorrectAnswer(Question question){
+        return question.getAnswerList().stream()
+                .filter(answer -> answer.getCorrect())
+                .map(answer -> answer.getName())
+                .findFirst().get();
+    }
+
+    public List<QuestionDto> mapToListQuestionDto(List<Question> questionList){
+        return questionList.stream()
+                .map(question -> mapToQuestionDto(question))
+                .collect(Collectors.toList());
+    }
 }
--- src/main/java/pl/rsjavasolutions/dtoquiz/model/EntityBase.java
+++ src/main/java/pl/rsjavasolutions/dtoquiz/model/EntityBase.java
@@ -12,4 +12,11 @@ public class EntityBase {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
